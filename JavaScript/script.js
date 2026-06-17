6efb66d828207850f6ceb0c572afa793a363d48b
jaogab <arthurkoch24@gmail.com>
2026-06-17 15:48:38 -0300

    CY2

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,1 +1,27 @@
+let forca = document.getElementById("forBase");
+let destreza = document.getElementById("desBase");
+let constituicao = document.getElementById("conBase");
+let inteligencia = document.getElementById("intBase");
+let sabedoria = document.getElementById("sabBase");
+let carisma = document.getElementById("carBase");
 
+let forD20 = document.getElementById("forD20")
+let desD20 = document.getElementById("desD20")
+let conD20 = document.getElementById("conD20")
+let intD20 = document.getElementById("intD20")
+let sabD20 = document.getElementById("sabD20")
+let carD20 = document.getElementById("carD20")
+
+let forMod = document.getElementById("forMod")
+let desMod = document.getElementById("desMod")
+let conMod = document.getElementById("conMod")
+let intMod = document.getElementById("intMod")
+let sabMod = document.getElementById("sabMod")
+let carMod = document.getElementById("carMod")
+
+let totalForca = document.getElementById('forTotal');
+let totalDestreza = document.getElementById('desTotal');
+let totalConstituicao = document.getElementById('conTotal');
+let totalInteligencia = document.getElementById('intTotal');
+let totalSabedoria = document.getElementById('sabTotal');
+let totalCarisma = document.getElementById('carTotal');
